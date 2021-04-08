24ed1072eb3258ce7b7b36488832f35c06dfd951
Dylan LaPierre <56058450+dylanlap98@users.noreply.github.com>
2021-04-08 19:47:33 -0400

    Add editorconfig rules & started gulp task HTMLValidator / HTMLCompress

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const { src, dest } = require(`gulp`);
+const htmlValidator = require(`gulp-html`);
+const htmlCompressor = require(`gulp-htmlmin`);
+
+let validateHTML = () => {
+    return src([
+        `html/*.html`,
+        `html/**/*.html`])
+        .pipe(htmlValidator());
+};
+
+let compressHTML = () => {
+    return src([`html/*.html`,`html/**/*.html`])
+        .pipe(htmlCompressor({collapseWhitespace: true}))
+        .pipe(dest(`prod`));
+};
+
+exports.validateHTML = validateHTML;
+exports.compressHTML = compressHTML;
