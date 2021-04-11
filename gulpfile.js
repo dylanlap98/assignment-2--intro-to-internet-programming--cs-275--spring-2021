45984fa992a90a690834fd3338d069c9d62322d8
Dylan LaPierre <56058450+dylanlap98@users.noreply.github.com>
2021-04-11 18:55:37 -0400

    Add gulp serve task with jslinter / reload func.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
-const { src, dest, series } = require(`gulp`);
+const { src, dest, watch, series } = require(`gulp`);
 const htmlValidator = require(`gulp-html`);
 const htmlCompressor = require(`gulp-htmlmin`);
 const cssLinter = require(`gulp-stylelint`);
+const browserSync = require(`browser-sync`);
+const jsLinter = require(`gulp-eslint`);
+const reload = browserSync.reload;
+
 
 let validateHTML = () => {
     return src([
@@ -26,7 +30,57 @@ let lintCSS = () => {
         }));
 };
 
+let lintJS = () => {
+    return src(`js/*.js`)
+        .pipe(jsLinter({
+            parserOptions: {
+                ecmaVersion: 2017,
+                sourceType: `module`
+            },
+            rules: {
+                indent: [2, 4, {SwitchCase: 1}],
+                quotes: [2, `backtick`],
+                semi: [2, `always`],
+                'linebreak-style': [2, `unix`],
+                'max-len': [1, 85, 4]
+            },
+            env: {
+                es6: true,
+                node: true,
+                browser: true
+            },
+            extends: `eslint:recommended`
+        }))
+        .pipe(jsLinter.formatEach(`compact`, process.stderr));
+};
+
+let serve = () => {
+    browserSync({
+        notify: true,
+        reloadDelay: 0,
+        server: {
+            baseDir: [
+                `html`,
+                `css`,
+            ]
+        }
+    });
+
+    watch(`js/*.js`,
+        series(lintJS)
+    ).on(`change`, reload);
+
+    watch(`css/*.css`,
+        series(lintCSS)
+    ).on(`change`, reload);
+
+    watch(`html/*.html`,
+        series(validateHTML, compressHTML)
+    ).on(`change`, reload);
+};
+
 exports.validateHTML = validateHTML;
 exports.compressHTML = compressHTML;
 exports.lintCSS = lintCSS;
-exports.runAll = series(validateHTML, compressHTML, lintCSS);
+exports.lintJS = lintJS;
+exports.serve = series(lintJS, validateHTML, compressHTML, lintCSS, serve);
